b00d083538560a70e0e42a6ca12e81a2871d45f0
“Daniel579” <seven579@hotmail.com>
2018-08-15 23:22:41 +0800

    Initial Commit

---
+++ ParkingAreaForJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParkingAreaForJSON
+//
+//  Created by HsiaoHsien Huang on 2018/8/15.
+//  Copyright © 2018年 HsiaoHsien Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
